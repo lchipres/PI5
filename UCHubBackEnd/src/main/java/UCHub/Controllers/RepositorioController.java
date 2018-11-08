d34e34160505fa30101141df7006ac4465a21b7a
Gerald Amezcua <gamezcua1@ucol.mx>
2018-11-07 22:42:57 -0600

    PUT repo

--- UCHubBackEnd/src/main/java/UCHub/Controllers/RepositorioController.java
+++ UCHubBackEnd/src/main/java/UCHub/Controllers/RepositorioController.java
@@ -97,7 +97,7 @@ public class RepositorioController {
         return repositorioRepository.findAll();
     }
 
-    @PutMapping(path="/{id}")
+    @PutMapping(path="/{id}/edit")
     public @ResponseBody RepositorioModel editRepository(@PathVariable(value="id") Integer id,
                                                          @RequestBody Map<String, String> body){
         Optional<RepositorioModel> r = repositorioRepository.findById((long)id);
